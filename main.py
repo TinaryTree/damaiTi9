0febaf1f1f3b6629450189d76e177be2778ab19f
q526933815 <32566921+q526933815@users.noreply.github.com>
2019-05-23 11:13:08 +0800

    更新readme

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import configparser
-import os
 
 
 class App:
