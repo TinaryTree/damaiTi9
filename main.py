bc48a2a76ddfd0a527ab4a7df1d1cc949dcdf8b0
q526933815 <32566921+q526933815@users.noreply.github.com>
2019-05-23 10:06:33 +0800

    升级

--- main.py
+++ main.py
@@ -22,27 +22,35 @@ class App:
         # self.driver.find_element_by_xpath('//*[@id="fm-login-password"]').send_keys(self.passwd)
 
         WebDriverWait(self.driver, 3000).until(
-            EC.presence_of_element_located((By.XPATH, '//div[@class="span-box-header name-user show"]')))
+            EC.presence_of_element_located((By.XPATH, '//a[@data-spm="duserinfo"]/div')))
         print('登陆成功')
-        user_name = self.driver.find_element_by_css_selector(
-            "body > div.dm-header-wrap > div > div.right-header > div.box-header.user-header > a.J_userinfo_name > div").text
+        user_name = self.driver.find_element_by_xpath('//a[@data-spm="duserinfo"]/div').text
         print('账号：', user_name)
-        self.driver.get('https://detail.damai.cn/item.htm?spm=a2oeg.home.card_2.ditem_0.591b23e1YhPKCw&id=593089517773')
+        self.driver.get('https://detail.damai.cn/item.htm?id=593089517773')
         WebDriverWait(self.driver, 10).until(
             EC.presence_of_element_located((By.XPATH, '//button[@data-spm="dconfirm"]')))
 
         dconfirm_button = self.driver.find_element_by_xpath('//button[@data-spm="dconfirm"]')
         while dconfirm_button.get_attribute('class') == 'privilege_sub disabled':
-            print('确定按钮无法点击，刷新页面')
-            self.driver.refresh()
+            print(dconfirm_button.get_attribute('class'), '确定按钮无法点击，刷新页面')
+            # self.driver.refresh()
             WebDriverWait(self.driver, 10).until(
                 EC.presence_of_element_located((By.XPATH, '//button[@data-spm="dconfirm"]')))
-            dconfirm_button = self.driver.find_element_by_xpath('//button[@data-spm="dconfirm"]')
+            try:
+                dconfirm_button = self.driver.find_element_by_xpath('//button[@data-spm="dconfirm"]')
+            except Exception as e:
+                print('寻找按钮失败', e)
 
-        self.driver.find_element_by_css_selector("#privilege_val").send_keys(self.dotakey)
+        self.driver.find_element_by_css_selector("#privilege_val").send_keys('tt')
         dconfirm_button.click()
+        try:
+            self.driver.find_element_by_xpath(
+                '//a[@class="cafe-c-input-number-handler cafe-c-input-number-handler-up"]').click()
+        except Exception as e:
+            print("未成功点击+号", e)
         self.driver.find_element_by_xpath('//div[@class="select_right_list"]/div[3]/span[2]').click()
-        dbuy_button = self.driver.find_element_by_xpath('//button[@data-spm="dbuy"]')
+        dbuy_button = self.driver.find_element_by_xpath('//div[@data-spm="dbuy"]')
+        print('寻找按钮:', dbuy_button.text)
         dbuy_button.click()
 
 
